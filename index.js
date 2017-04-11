92fed4ab050c28a65ae5bc0a442dfd2012212346
Jeff Katz <jeffreyerickatz@gmail.com>
2017-04-10 23:10:02 -0400

    adding solution

--- index.js
+++ index.js
@@ -1,59 +1,38 @@
-function printString(string){
-  console.log(string[0])
-  if(string.length > 1){
-    let substring = string.substring(1, string.length)
-    printString(substring)
-  } else{
-    return true
-  }
+function depthFirstSearch(rootNode, vertices, edges){
+		let stack = []
+		stack.push(rootNode)
+		console.log(rootNode.name)
+		while(stack.length != 0){
+			// console.log(`first element in stack is ${stack[0].name}`)
+			let v = stack.pop()
+			if(!v.discovered){
+				v.discovered = true
+
+				findAdjacent(v.name, vertices, edges).forEach(function(node){
+
+					console.log(node.name)
+					stack.push(node)
+				})
+			}
+		}
 }
 
-function reverseString(string){
-  // pizza
-  // a +
-  if(string.length > 1){
-    let substring = string.substring(0, string.length -1)
-    return string[string.length -1] + reverseString(substring)
-  } else {
-    return string
-  }
+function findAdjacent(nodeName,  vertices, edges){
+  return edges.filter(function(edge){
+    return edge.includes(nodeName)
+  }).map(function(edge){
+    return edge.filter(function(node){
+      return (node != nodeName)
+    })[0]
+  }).map(function(name){
+    return findNode(name, vertices)
+  }).filter(function(node){
+		return !node.discovered
+	})
 }
 
-
-function isPalindrome(string){
-  if(string.length < 2){
-    return true
-  } else {
-    let substring = string.substring(1, string.length - 1)
-    return (string[0] == string[string.length -1] && isPalindrome(substring))
-  }
-}
-
-function addUpTo(array, index){
-  if(index === 0){
-    return array[0]
-  } else {
-    return addUpTo(array, index-1) + array[index]
-  }
-}
-
-
-function maxOf(array){
-  if(array.length == 1){
-    return array[0]
-  } else {
-    let subArray = array.slice(0, array.length -1)
-    let currentMax = maxOf(subArray)
-    let newMax = currentMax > array[array.length -1] ? currentMax : array[array.length -1]
-    return newMax;
-  }
-}
-
-function includesNumber(array, number){
-  if(array.length < 2){
-    return (array[0] == number)
-  }else{
-    let subarray = array.slice(0, array.length - 1)
-    return(includesNumber(subarray, number) || (array[array.length -1] == number))
-  }
+function findNode(nodeName, vertices){
+  return vertices.find(function(vertex){
+    return vertex.name == nodeName
+  })
 }
